a84f981ee97c1b0357aa7536d55b13f93f9781f2
Isaac Spiegel <isaacspiegel@gmail.com>
2023-06-09 18:19:12 -0500

    Frontend

---
+++ frontend/src/pages/api/token.ts
@@ -0,0 +1,47 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { AccessToken } from 'livekit-server-sdk'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  token: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { 
+    query: {
+      jane,
+    }
+  } = req
+
+  const token = new AccessToken(
+    'key',
+    'secret',
+    {
+      ttl: '1 days',
+      name: jane ? 'Jane Doe' : 'John Doe',
+      identity: jane ? 'jane-doe' : 'john-doe',
+    }
+  );
+
+  token.addGrant({
+    roomCreate: true,
+    roomJoin: true,
+    roomList: true,
+    roomRecord: true,
+    roomAdmin: true,
+    room: 'Default',
+    ingressAdmin: true,
+    canPublish: true,
+    // canPublishSources: true,
+    canSubscribe: true,
+    canPublishData: true,
+    canUpdateOwnMetadata: true,
+    hidden: true,
+    recorder: true,
+  });
+
+  res.status(200).json({ token: token.toJwt() })
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,17 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
+import { LiveKitRoom, VideoConference } from '@livekit/components-react'
+import '@livekit/components-styles'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({
+  token,
+  tokenJane,
+}: {
+  token: string,
+  tokenJane: string,
+}) {
   return (
     <>
       <Head>
@@ -12,6 +20,56 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <table>
+        <thead>
+          <tr>
+            <th>Token</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{token}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: '2rem',
+        }}
+      >
+        <LiveKitRoom
+          serverUrl="ws://localhost:7880"
+          token={token}
+        >
+          <VideoConference />
+        </LiveKitRoom>
+        <LiveKitRoom
+          serverUrl="ws://localhost:7880"
+          token={tokenJane}
+        >
+          <VideoConference />
+        </LiveKitRoom>
+      </div>
     </>
   )
+}
+
+export async function getServerSideProps() {
+  const token = await fetch('http://localhost:3000/api/token')
+    .then((res) => res.json())
+    .then((data) => data.token)
+
+  const tokenJane = await fetch('http://localhost:3000/api/token?jane=true')
+    .then((res) => res.json())
+    .then((data) => data.token)
+
+  return {
+    props: {
+      token,
+      tokenJane,
+    },
+  }
 }
